5846c1e75454ef847e5184c50194d5ebe7a397e7
Ivan Arrizabalaga Getino <ivan.arrizabalaga@gmail.com>
2013-06-24 13:05:00 +0200

    Griffon 1.3.0  + No inheritance

--- griffon-app/controllers/testthread/SampleDialogController.groovy
+++ griffon-app/controllers/testthread/SampleDialogController.groovy
@@ -1,12 +1,79 @@
 package testthread
 
-class SampleDialogController extends BaseDialogController{
+import griffon.transform.Threading
+import java.awt.Window
+import java.awt.event.WindowListener
+import javax.swing.JDialog
+
+
+class SampleDialogController{
     def model
     def view
+	def builder
+
+	protected dialog
+	
 
 	void mvcGroupInit(def args) {
 		super.mvcGroupInit(args)
+	}		
+
+	@Threading(Threading.Policy.INSIDE_UITHREAD_SYNC)
+	def show = { Window window = null ->
+		window = window ?: Window.windows.find{ it.focused }
+		if(!dialog || dialog.owner != window) {
+			dialog = builder.dialog(
+					owner: window,
+					title: model.title,
+					resizable: model.resizable,
+					modal: model.modal,
+					defaultCloseOperation: JDialog.DO_NOTHING_ON_CLOSE)
+					{ container(view.dialog) }
+			if(model.width > 0 && model.height > 0) {
+				dialog.preferredSize = [model.width, model.height]
+			}
+			dialog.addWindowListener(windowListener)
+			dialog.pack()
+		}
+		int x = window.x + (window.width - dialog.width) / 2
+		int y = window.y + (window.height - dialog.height) / 2
+				
+		dialog.setLocation(x, y)				
+		dialog.visible = true
 	}
+
+	@Threading(Threading.Policy.INSIDE_UITHREAD_SYNC)
+	def hide = { evt = null ->
+		dialog?.visible = false
+		dialog?.dispose()
+		dialog = null
+	}
+	
+	@Threading(Threading.Policy.INSIDE_UITHREAD_SYNC)
+	def okAction = {
+		hide()
+	}
+	
+	@Threading(Threading.Policy.INSIDE_UITHREAD_SYNC)
+	def closeAction = {
+		model.closed = true
+		hide()
+	}
+	
+	def windowListener = [
+		windowClosed: { },
+		windowOpened: {},
+		windowClosing: { event -> closeAction() },
+		windowIconified: { },
+		windowDeiconified: { },
+		windowActivated: { },
+		windowDeactivated: { }
+	 ] as WindowListener
+	
+ 	@Threading(Threading.Policy.SKIP)
+	def isClosed={
+		 return model.closed
+	}	
 	
 	def click1Action={ evt=null ->
 		log.info evt.source.text
--- griffon-app/models/testthread/SampleDialogModel.groovy
+++ griffon-app/models/testthread/SampleDialogModel.groovy
@@ -1,10 +1,25 @@
 package testthread
 
+import griffon.util.GriffonNameUtils
 import groovy.beans.Bindable
 
-class SampleDialogModel extends BaseDialogModel{
+import javax.swing.Icon
+
+class SampleDialogModel{
 	
 	@Bindable String clickedText
+	def view
+
+	@Bindable String title="No title"
+	@Bindable int width = 0
+	@Bindable int height = 0
+	@Bindable int maxStatus = 0
+	@Bindable boolean resizable = false
+	@Bindable boolean modal = true
+	@Bindable String statusMessage
+	@Bindable Icon statusIcon
+	@Bindable boolean cancelable=false		
+	@Bindable boolean closed = false
 	
 	SampleDialogModel() {
 		super()
@@ -12,7 +27,21 @@ class SampleDialogModel extends BaseDialogModel{
 		height = 150
 		maxStatus = 75
 		cancelable=true
+	}	
+
+	void mvcGroupInit(Map<String, Object> args) {
+		title = GriffonNameUtils.capitalize(dialogTitle)
+		statusMessage = " "
+		statusIcon=null
 	}
+
+	def getStatusMessage(){
+		if (this.statusMessage.length() > maxStatus){
+			return this.statusMessage.substring(0, maxStatus)+" ..."
+		}
+		
+		return this.statusMessage
+	}	
 
 	@Override
 	protected String getDialogTitle() {
